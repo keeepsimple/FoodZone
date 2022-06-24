2b966cd963a66db42038b39a201a5df5091ffb09
QuangNV <quangbeo199x@gmail.com>
2022-06-24 16:39:57 +0700

    Update Display Menu

--- src/FoodZone/FoodZone.Services/Services/FoodServices.cs
+++ src/FoodZone/FoodZone.Services/Services/FoodServices.cs
@@ -2,6 +2,8 @@
 using FoodZone.Models.Common;
 using FoodZone.Services.BaseServices;
 using FoodZone.Services.IServices;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FoodZone.Services.Services
 {
@@ -9,6 +11,11 @@ namespace FoodZone.Services.Services
     {
         public FoodServices(IUnitOfWork unitOfWork) : base(unitOfWork)
         {
+        }
+
+        public IEnumerable<Food> GetFoodByMenu(int menuId)
+        {
+            return _unitOfWork.FoodRepository.GetQuery(f => f.MenuFoods.Any(m => m.MenuId == menuId)).ToList();
         }
     }
 }
