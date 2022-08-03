fcba37fdda9d770759f343e3acc761025d4b57f2
hungvthe130522 <83944478+hungvthe130522@users.noreply.github.com>
2022-08-03 20:32:27 +0700

    Update NewsAdminController

--- src/FoodZone/FoodZone.Web/Areas/Admin/Controllers/NewsAdminController.cs
+++ src/FoodZone/FoodZone.Web/Areas/Admin/Controllers/NewsAdminController.cs
@@ -24,6 +24,141 @@ namespace FoodZone.Web.Areas.Admin.Controllers
             _newsServices = newServices;
         }
 
-        
+        [HttpGet]
+        public async Task<ActionResult> Index(string searchString, string currentFilter, int? page)
+        {
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var news = await _newsServices.GetAllAsync();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                news = news.Where(s => s.Title.Contains(searchString)).ToList();
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(news.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult Create()
+        {
+            var newsViewModel = new NewsViewModel();
+            return View(newsViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public async Task<ActionResult> Create(NewsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var news = new News
+                {
+                    Title = model.Title,
+                    Content = model.Content,
+                    Image = model.Image,
+                    ShortDescription = model.ShortDescription
+                };
+
+                var result = await _newsServices.AddAsync(news);
+                if (result > 0)
+                {
+                    TempData["Message"] = "Tạo thành công.";
+                }
+                else
+                {
+                    TempData["Message"] = "Tạo thất bại. Thử lại sao nhé!";
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var news = _newsServices.GetById((int)id);
+
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+
+            var model = new NewsViewModel()
+            {
+                Id = news.Id,
+                Title = news.Title,
+                Content = news.Content,
+                Image = news.Image,
+                ShortDescription = news.ShortDescription
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public async Task<ActionResult> Edit(NewsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var news = _newsServices.GetById(model.Id);
+                if(news == null)
+                {
+                    return HttpNotFound();
+                }
+
+                news.Title = model.Title;
+                news.ShortDescription = model.ShortDescription;
+                news.Content = model.Content;
+                news.Image = model.Image;
+
+                var result = await _newsServices.UpdateAsync(news);
+
+                if (result)
+                {
+                    TempData["Message"] = "Cập nhật thành công.";
+                }
+                else
+                {
+                    TempData["Message"] = "Cập nhật thất bại.";
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var result = _newsServices.Delete(id);
+            if (result)
+            {
+                TempData["Message"] = "Xóa thành công.";
+            }
+            else
+            {
+                TempData["Message"] = "Xóa thất bại.";
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
