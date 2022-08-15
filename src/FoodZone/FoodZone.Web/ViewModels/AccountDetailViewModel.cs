4c2f1c34135caee23fb92799d9100c4575bbb1fd
QuangNV <quangbeo199x@gmail.com>
2022-08-15 21:49:45 +0700

    Update User profile 2

--- src/FoodZone/FoodZone.Web/ViewModels/AccountDetailViewModel.cs
+++ src/FoodZone/FoodZone.Web/ViewModels/AccountDetailViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace FoodZone.Web.ViewModels
 {
@@ -9,12 +11,21 @@ namespace FoodZone.Web.ViewModels
     {
         public string UserId { get; set; }
 
+        
         public string Username { get; set; }
 
+        [Required(ErrorMessage = "{0} không được bỏ trống")]
+        [Display(Name = "Tên đầy đủ")]
         public string FullName { get; set; }
 
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Required]
+        [Phone]
+        [Display(Name = "Số điện thoại")]
         public string PhoneNumber { get; set; }
 
         public int Level { get; set; }
