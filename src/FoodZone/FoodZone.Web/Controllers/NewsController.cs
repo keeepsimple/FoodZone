3aa2b54e3966c6af9d43d709f4ede1f68ed7d343
hoanganh21 <hoanganh2010999@gmail.com>
2022-08-18 14:23:07 +0700

    Update NewsAdminController

--- src/FoodZone/FoodZone.Web/Controllers/NewsController.cs
+++ src/FoodZone/FoodZone.Web/Controllers/NewsController.cs
@@ -12,47 +12,6 @@ namespace FoodZone.Web.Controllers
 {
     public class NewsController : Controller
     {
-        private readonly INewsServices _newsServices;
-
-        public NewsController(INewsServices newServices)
-        {
-            _newsServices = newServices;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult> Index(string searchString, string currentFilter, int? page)
-        {
-            if (searchString != null)
-            {
-                page = 1;
-            }
-            else
-            {
-                searchString = currentFilter;
-            }
-
-            ViewBag.CurrentFilter = searchString;
-
-            var news = await _newsServices.GetAllAsync();
-            if (!string.IsNullOrEmpty(searchString))
-            {
-                news = news.Where(s => s.Title.Contains(searchString)).ToList();
-            }
-
-            int pageSize = 10;
-            int pageNumber = (page ?? 1);
-            return View(news.ToPagedList(pageNumber, pageSize));
-        }
-
-        public ActionResult Details(int newsId)
-        {
-            var news = _newsServices.GetById(newsId);
-            if (news == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.News = news.Title;
-            return View(news);
-        }
+        
     }
 }
