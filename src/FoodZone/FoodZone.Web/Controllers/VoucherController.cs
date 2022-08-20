3cb9b220d909dffc3e32cb1cf8dd8ae2160245f0
ducnphe131008 <ducnphe131008@fpt.edu.vn>
2022-08-21 00:16:28 +0700

    Update ForgotPasswordAccountController

--- src/FoodZone/FoodZone.Web/Controllers/VoucherController.cs
+++ src/FoodZone/FoodZone.Web/Controllers/VoucherController.cs
@@ -12,6 +12,48 @@ namespace FoodZone.Web.Controllers
 {
     public class VoucherController:Controller
     {
-        
+        private readonly IVoucherServices _voucherServices;
+
+        public VoucherController(IVoucherServices voucherServices)
+        {
+            _voucherServices = voucherServices;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index(string searchString, string currentFilter, int? page)
+        {
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var vouchers = await _voucherServices.GetAllAsync();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                vouchers = vouchers.Where(s => s.Title.Contains(searchString)).ToList();
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(vouchers.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult Details(int voucherId)
+        {
+            var voucher = _voucherServices.GetById(voucherId);
+            if (voucher == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Voucher = voucher.Title;
+            return View(voucher);
+
+        }
     }
 }
